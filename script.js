18e73fea390129450fd82cd31e19715b2679271a
Wodoame <bmwodoame@gmail.com>
2025-08-24 03:59:35 +0000

    Auto-commit: 24/08/2025  3:59:35.39

---
+++ script.js
@@ -0,0 +1,19 @@
+const db = [
+    { id: 1, name: "Item 1", price: 10 },
+    { id: 2, name: "Item 2", price: 20 },
+    { id: 3, name: "Item 3", price: 30 }
+];
+
+document.addEventListener("alpine:init", () => {
+    
+    // create an alpine component
+
+    Alpine.data("dropdown", () => ({
+        db: db,
+        open: false,
+        toggle() {
+            this.open = !this.open;
+        },
+    }));
+
+});

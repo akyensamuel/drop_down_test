83fcdcadfe3cef999a90f9958334506a7c98aa94
Wodoame <bmwodoame@gmail.com>
2025-08-24 12:00:47 +0000

    format index.html and script.js with Prettier

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const db = [
-    { id: 1, name: "Item 1", price: 10 },
-    { id: 2, name: "Item 2", price: 20 },
-    { id: 3, name: "Item 3", price: 30 }
+  { id: 1, name: "Item 1", price: 10 },
+  { id: 2, name: "Item 2", price: 20 },
+  { id: 3, name: "Item 3", price: 30 },
 ];
 
 document.addEventListener("alpine:init", () => {
-    
-    // create an alpine component
+  // create an alpine component
 
-    Alpine.data("dropdown", () => ({
-        db: db,
-        filtered: [], 
-        open: false,
-        init(){
-            this.filtered = [...this.db];
-        },
-        toggle() {
-            this.open = !this.open;
-        },
-        filter(query){
-            this.filtered = this.db.filter(item => item.name.toLowerCase().includes(query.toLowerCase()));
-            console.log(query);
-        },
-        Select(item, inputField){
-            this.open = false;
-            inputField.value = item.name;
-        }
-    }));
-
+  Alpine.data("dropdown", () => ({
+    db: db,
+    filtered: [],
+    open: false,
+    init() {
+      this.filtered = [...this.db];
+    },
+    toggle() {
+      this.open = !this.open;
+    },
+    filter(query) {
+      this.filtered = this.db.filter((item) =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+      console.log(query);
+    },
+    Select(item, inputField) {
+      this.open = false;
+      inputField.value = item.name;
+    },
+  }));
 });
